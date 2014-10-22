d328e7e6f2ff07aba21426f5a10fcb13e7a8d86d
chadd <chadd@pacificu.edu>
2014-10-22 08:33:45 -0700

    add requests Example

---
+++ LectureExamples/requestsExample.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+################################
+# File Name:	requestsExample.py
+# Author:		Chadd Williams
+# Date:			10/20/2014
+# Class:		CS 360
+# Assignment:	Lecture Examples
+# Purpose:		a deck of cards to demonstrate unittests
+################################
+
+#  pip-3.3 install requests
+
+# python3 requestsExample.py
+
+import requests
+import json
+import pprint
+
+# fetch data from the web (this is JSON data)
+r = requests.get("https://api.github.com/users/cs360f14")
+
+# build a decoder
+d = json.JSONDecoder()
+
+# transform JSON to a dictionary
+data = d.decode(r.text)
+
+# build a pretty printer
+pp = pprint.PrettyPrinter(indent=2)
+
+# display nicely
+pp.pprint(data)
+
+

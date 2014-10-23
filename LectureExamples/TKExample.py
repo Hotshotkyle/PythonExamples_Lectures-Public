46859a90960b68ca60092b7d4eb6d6c04483052c
chadd <chadd@pacificu.edu>
2014-10-23 08:54:27 -0700

    add more buttons and button handlers to more clearly show the grid

--- LectureExamples/TKExample.py
+++ LectureExamples/TKExample.py
@@ -13,11 +13,35 @@ from tkinter import *
 from tkinter.ttk import *
 
 
+class ButtonHandler:
+	""" simple class that will provide methods to handle button clicks
+	"""
+	
+	def __init__(self):
+		self._counter = 0				
+				
+	def printButtonClickCounter(self):
+		print("Counter:", self._counter)
+		
+	def wowButton(self):
+		self._counter += 1
+		print("You pressed the WOW button")
+		self.printButtonClickCounter()
 
+	def pacificButton(self):
+		self._counter += 1
+		print("You pressed the Pacific button")
+		self.printButtonClickCounter()
+
+
+# a callback function does not need to be part of a class
 def message():
 	""" callback function to print OK when the button is pressed """
 	print("OK")
 
+
+
+handler = ButtonHandler()
 
 # build the root
 root = Tk()
@@ -34,6 +58,21 @@ okButton=Button(master=cF, text="OK",command=message)
 
 # place the button the grid
 okButton.grid(column=0,row=0)
+
+
+# create a button, hooked up the Frame, with the text OK
+# and the button press goes to the function message()
+wowButton=Button(master=cF, text="Wow",command=handler.wowButton)
+
+# place the button the grid
+wowButton.grid(column=1,row=0)
+
+# create a button, hooked up the Frame, with the text OK
+# and the button press goes to the function message()
+pacificButton=Button(master=cF, text="Pacific",command=handler.pacificButton)
+
+# place the button the grid
+pacificButton.grid(column=1,row=1)
 
 # run the UI loop
 root.mainloop()

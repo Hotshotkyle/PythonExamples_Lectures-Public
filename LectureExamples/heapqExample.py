289a80eeefd4ddb6e2b52c146be13493fac16acf
chadd <chadd@pacificu.edu>
2014-10-30 16:45:24 -0700

    added csv, datetime, heapq examples

---
+++ LectureExamples/heapqExample.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+########################################################################
+# File Name:  	heapqExample.py
+# Author:		chadd williams
+# Date:			Oct 30, 2014
+# Class:		CS 360
+# Assignment:	Example heapq
+# Purpose:		Show examples of using heapq
+########################################################################
+
+
+from heapq import *
+
+# https://docs.python.org/3/library/heapq.html
+
+
+# create the heap as an empty list
+theHeap = []
+
+for x in reversed(range(9)):
+	print(x)
+	heappush(theHeap, x)
+
+# The smallest item is at the top of the list
+print(theHeap)
+print('--------------')
+
+
+# a heap does not provide an iterator!
+# the following is an unusual use of a heap for 
+# demonstration purposes only
+
+heapIsEmpty = False
+
+while not heapIsEmpty:
+
+	# pop the smallest item off the top of the heap!
+	try:
+		x = heappop(theHeap)
+	except IndexError:
+		# when the heap is empty, heappop() raises IndexError
+		# catch the exception and break out of the loop
+		heapIsEmpty = True
+		
+	print(x)
